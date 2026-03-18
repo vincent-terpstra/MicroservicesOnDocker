6e03ccd2c9dda3b61378635a146d96176a707f78
Vincent Terpstra <vdterpst@gmail.com>
2026-03-18 12:48:07 -0400

    Update `CommandsService` assertions to expect 200 status, add Kubernetes script for Docker image deployment and rollout, and set `imagePullPolicy` to `Always` in `commands-depl.yaml`.

---
+++ K8S/Powershell/UpdateKubernetes.ps1
@@ -0,0 +1,8 @@
+﻿docker build -t douglasvincent/commandservice ../../CommandsService/
+docker build -t douglasvincent/platformservice ../../PlatformService/
+
+docker push  douglasvincent/platformservice
+docker push  douglasvincent/commandservice
+
+kubectl rollout restart deployment commands-depl
+kubectl rollout restart deployment platforms-depl
